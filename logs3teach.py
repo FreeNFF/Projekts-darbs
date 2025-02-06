b21b5d1fe234ed0a6ed08a14a62b51d8500e4ac6
FreeNFF <nauisfricsons30@gmail.com>
2025-02-06 13:13:11 +0200

    commit

--- logs3teach.py
+++ logs3teach.py
@@ -5,21 +5,12 @@ from tkinter import messagebox
 import subprocess
 import calendar
 from datetime import date
-from tkcalendar import Calendar #+ terminālī pip install tkcalendar
+from tkcalendar import Calendar
 
 Logs3t=Tk()
 Logs3t.title("Kalendārs")
 Logs3t.geometry("300x500")
 Logs3t.configure(background="#f9f9f9")
-
-def grad_date():
-    Logs3t.destroy()
-    subprocess.call(['python', 'logs4teach.py'])
-
-
-
-
-
 
 sodien = date.today()
 sodiend = int(sodien.strftime('%d'))
@@ -31,23 +22,31 @@ cal = Calendar(Logs3t, selectmode = 'day',
                year = sodieng, month = sodienm,
                day= sodiend)
  
+
+
 cal.grid(padx=25, pady = 20)
+
 
 choosen_date= cal.get_date()
 
 
-
+def grad_date():
+    Logs3t.destroy()
+    subprocess.call(['python', 'logs4teach.py'])
 
 
 
  
-# Add Button and Label
-Button(Logs3t, text = "Izvēlēties datumu", font="Arial",bd=5, command = grad_date).grid(pady = 20)
 
-tk.Button(Logs3t, text="Atgriezties",font="Arial",bd=5, command=lambda:uzlogu1()).grid(row=3, column=0, padx=20, pady=20)
+Button(Logs3t, text = "Izvēlēties datumu", font="Arial",bd=5, command =lambda: grad_date()).grid(pady = 20)
 
-def uzlogu1():
+tk.Button(Logs3t, text="Atgriezties",font="Arial",bd=5, command=lambda:uzlogu2t()).grid(row=3, column=0, padx=20, pady=20)
+
+def uzlogu2t():
     Logs3t.destroy()
     subprocess.call(['python', 'logs1teach.py'])
+
+
+
 
 Logs3t.mainloop()
