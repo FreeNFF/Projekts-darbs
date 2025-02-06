3f5a63a50a36cd9de4046db411fa8e155f1554f8
FreeNFF <nauisfricsons30@gmail.com>
2025-02-06 10:55:24 +0200

    Co-authored-by: EDVARDS OTO VOICEHOVIČS <edvards.voicehovics@edu.kekava.lv>

--- aplikacija.py
+++ aplikacija.py
@@ -19,14 +19,6 @@ Logs.title("Mācību konsultācijas")
 Logs.geometry("300x500")
 Logs.configure(background="#f9f9f9")
 
-
-ttk.Label(Logs, text="Lietotājvārds",font="Arial 20",background='#f9f9f9').grid(row=2, column=1, padx=30, pady=40)
-username_entry = ttk.Entry(Logs, font="Arial",background='#f9f9f9')
-username_entry.grid(row=3, column=1, padx=30, pady=10)
-ttk.Label(Logs, text="Parole",font="Arial 20",background='#f9f9f9').grid(row=4, column=1, padx=30, pady=40)
-password_entry = ttk.Entry(Logs, font="Arial",background='#f9f9f9')
-password_entry.grid(row=5, column=1, padx=30, pady=10)
-
 def login():
 
     lietotaji={}
@@ -44,6 +36,16 @@ def login():
             #     subprocess.call(['python', 'logs1stud.py'])
             # else:
             #     messagebox.showerror("Kļūda", "Nepareizs lietotāja vārds vai parole!")
+
+
+ttk.Label(Logs, text="Lietotājvārds",font="Arial 20",background='#f9f9f9').grid(row=2, column=1, padx=30, pady=40)
+username_entry = ttk.Entry(Logs, font="Arial",background='#f9f9f9')
+username_entry.grid(row=3, column=1, padx=30, pady=10)
+ttk.Label(Logs, text="Parole",font="Arial 20",background='#f9f9f9').grid(row=4, column=1, padx=30, pady=40)
+password_entry = ttk.Entry(Logs, font="Arial",background='#f9f9f9')
+password_entry.grid(row=5, column=1, padx=30, pady=10)
+
+
 
 
 tk.Button(Logs, text="Pieslēgties",font="Arial",bd=5, command=login).grid(row=6, column=1, padx=100, pady=40)
