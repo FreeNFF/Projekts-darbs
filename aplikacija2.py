e326087928614abcd516498109f100b9de51ef95
FreeNFF <nauisfricsons30@gmail.com>
2025-03-06 12:17:49 +0200

    commit

--- aplikacija2.py
+++ aplikacija2.py
@@ -91,29 +91,32 @@ class CalendarApp:#izveido klasi
         
         
     def open_input_page(self):#uzpiežot pogu atveras labošanas vai ievadīšanas lapa 
-        #
+        #izveido jaunu logu
         input_window = tk.Toplevel(self.root)
         input_window.title("Pieteikties konsultācijai")
         input_window.geometry("300x500")
-        
+        #pievienu tekstus un kalendāru 
         ttk.Label(input_window, text="Izvēlieties datumu:").pack()
         input_calendar = Calendar(input_window, selectmode='day', date_pattern="mm/dd/yyyy")
         input_calendar.pack()
-        self.highlight_dates(input_calendar)
-        ttk.Label(input_window, text="Pieteikties konsultācijai (vārds, uzvārds, ko vēlies darīt)").pack()
+        self.highlight_dates(input_calendar)#pievieno funkciju, kura iezīmē konsultācijas datumus
+
+        #pievieno vietu, kur ievadīt konsultācijas informāciju
+        ttk.Label(input_window, text="Pieteikties konsultācijai (Vārds, uzvārds, ko vēlies darīt)").pack()
         text_entry = tk.Text(input_window, width=40, height=5)
         #os.environ["text_entry1"]=text_entry
         text_entry.pack()
 
         
-        
+        #ielādē iepriekš izveidotās konsultācijas, ja tādas ir.
         def load_existing_info():
             date = input_calendar.get_date()
             text_entry.delete("1.0", tk.END)
             text_entry.insert("1.0", self.data.get(date, ""))
         
-        load_existing_info()  # Parāda info izvēlētajam datumam
+        load_existing_info()#pievieno funkciju, kura ielādē iepriekš izveidotās konsultācijas, ja tādas ir
 
+        #funkcija, kura saglabā ievadītos datus teksta lodziņā
         def save_data():
             date = input_calendar.get_date()
             info = text_entry.get("1.0", tk.END).strip()
@@ -121,14 +124,13 @@ class CalendarApp:#izveido klasi
             if info:
                 self.data[date] = info
             else:
-                self.data.pop(date, None)  # Remove date if empty
-
+                self.data.pop(date, None)  # noņem iezīmēto datumu, ja nav nekas ierakstīts teksta lodziņā
             self.save_data()
             self.highlight_dates()
             messagebox.showinfo("Viss izdevies", "Informācija veiksmīgi saglabāta!")
             input_window.destroy()
         
-        input_calendar.bind("<<CalendarSelected>>", lambda e: load_existing_info())
+        input_calendar.bind("<<CalendarSelected>>", lambda e: load_existing_info())#Atjaunina teksta lauku, kad ir atlasīts jauns datums
         
         save_button = tk.Button(input_window, text="Saglabāt", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=save_data)#Informācijas saglabāšanas poga
         save_button.pack(pady=5)
@@ -137,7 +139,7 @@ class CalendarApp:#izveido klasi
 
 
 
-    def highlight_dates(self, calendar_widget=None):
+    def highlight_dates(self, calendar_widget=None):#funkcija, ar kuru konsultācijas datumus iezīmē dzeltenā krāsā
         calendar_widget = calendar_widget or self.calendar  
         calendar_widget.calevent_remove('all')
         calendar_widget.tag_config("atg", background="yellow", foreground="black")
@@ -153,18 +155,18 @@ class CalendarApp:#izveido klasi
         calendar_widget.update_idletasks()  
     
 
-    def save_data(self):
+    def save_data(self):#saglabā datus noteiktā failā
         with open(self.data_file, "w") as file:
             json.dump(self.data, file)
     
-    def load_data(self):
+    def load_data(self):#ielādē datus no šī paša faila
         try:
             with open(self.data_file, "r") as file:
                 return json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             return {}
 
-if __name__ == "__main__":
+if __name__ == "__main__":#funkcija ar kuras palīdzību tiek palaista programma
     root = tk.Tk()
     app = CalendarApp(root)
     root.mainloop()
--- aplikacija3.py
+++ aplikacija3.py
@@ -1,4 +1,4 @@
-
+#pievieno nepieciešamās bibliotēkas
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -16,20 +16,20 @@ class CalendarApp:#izveido klasi
         self.root = root#pārdēvē logu
         self.root.title("Konsultāciju kalendārs")#loga nosaukums
         root.geometry("300x500")#loga izmēri
-        self.root.overrideredirect(True)  
+        self.root.overrideredirect(True)#noņem sākotnējā loga dizainu
 
-        
+        #loga sākumpunkts
         self.start_x = 0
         self.start_y = 0
 
-        
+        #loga dizaina izveidošana
         self.title_bar = tk.Frame(root, bg="#98c41c", relief="raised", bd=2)
         self.title_bar.pack(side="top", fill="x")
 
         self.title_label = tk.Label(self.title_bar, text="Mācību konsultācijas", font=("Roboto", 12, "bold"), bg="#98c41c", fg="#f9f9f9")
         self.title_label.pack(side="left", padx=10)
 
-        self.close_button = tk.Button(self.title_bar, text="Aizvērt", bg="red", font=("Roboto", 8, "bold"), bd=3,fg="#f9f9f9", command=root.destroy)
+        self.close_button = tk.Button(self.title_bar, text="Aizvērt", bg="red", font=("Roboto", 8, "bold"), bd=3,fg="#f9f9f9", command=root.destroy)#Poga ar kuru aizver visu logu
         self.close_button.pack(side="right", padx=5,pady=1)
 
         
@@ -39,7 +39,7 @@ class CalendarApp:#izveido klasi
         self.data_file = "calendar_data.json"#atver jau ievadītos vai izveido jaunu failu kur saglabāsies dati
         self.data = self.load_data()#pievieno funkciju., kurā ielādē jau ievadītos datus
         
-        self.create_main_page()#paša loga izveides funkcija
+        self.create_main_page()#loga izveides funkcija
 
     def start_move(self, event):
         self.start_x = event.x_root - self.root.winfo_x()
@@ -59,15 +59,15 @@ class CalendarApp:#izveido klasi
         self.calendar = Calendar(frame, selectmode='day', date_pattern="mm/dd/yyyy")#kalendāru ievieto rāmi(izveido kalendāru)
         self.calendar.pack()
         
-        self.highlight_dates()
+        self.highlight_dates()#funkcija kas iekrāso kalendārā datumus
 
         view_button = tk.Button(frame, text="Apskatīt konsultāciju", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=self.view_details)#Poga info apskatīšanai
         view_button.pack(pady=20)
         
-        add_button = tk.Button(frame, text="Pievienot/Labot konsultāciju", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=self.open_input_page)
+        add_button = tk.Button(frame, text="Pievienot/Labot konsultāciju", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=self.open_input_page)#Poga jaunas konsultācijas izveidošanai
         add_button.pack(pady=20)
         
-        izrakstisanas = tk.Button(root, text="Izrakstīties", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=self.uzlogu1)
+        izrakstisanas = tk.Button(root, text="Izrakstīties", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=self.uzlogu1)#Poga lai atgrieztos uz pieslēgšanās logu
         izrakstisanas.pack(pady=20)#Izrakstīšanās poga
         
     def uzlogu1(self):#Funkcija izrakstīšanās pogai
@@ -83,22 +83,22 @@ class CalendarApp:#izveido klasi
         input_calendar = Calendar(input_window, selectmode='day', date_pattern="mm/dd/yyyy")
         input_calendar.pack()
         self.highlight_dates(input_calendar)
-        ttk.Label(input_window, text="Pievienot informāciju (Vards, uzvārds, laiks, kabinets,").pack()
-        ttk.Label(input_window, text="Skolēni:):").pack()
+        ttk.Label(input_window, text="Pievienot informāciju (Vārds, uzvārds, laiks, kabinets,").pack()
+        ttk.Label(input_window, text="Skolēni: ):").pack()
         text_entry = tk.Text(input_window, width=40, height=5)
         #os.environ["text_entry1"]=text_entry
         text_entry.pack()
 
         
         
-        def load_existing_info():
+        def load_existing_info():#funkcija
             date = input_calendar.get_date()
             text_entry.delete("1.0", tk.END)
             text_entry.insert("1.0", self.data.get(date, ""))
         
         load_existing_info()  # Parāda info izvēlētajam datumam
         
-        def save_data():
+        def save_data():#Funkcija "saglabāt" pogai
             date = input_calendar.get_date()
             info = text_entry.get("1.0", tk.END).strip()
 
@@ -119,7 +119,7 @@ class CalendarApp:#izveido klasi
 
     
     
-    def view_details(self):
+    def view_details(self):#Funkcija pogai "apskatīt konsultāciju"
         selected_date = self.calendar.get_date()
         info = self.data.get(selected_date, "Nav konsultācija šajā datumā!")
         
@@ -132,7 +132,7 @@ class CalendarApp:#izveido klasi
         
         tk.Button(details_window, text="Aizvērt", font=("Roboto",10,"bold"),bg="#98c41c",fg="white",bd=3, command=details_window.destroy).pack(pady=20)
         
-    def highlight_dates(self, calendar_widget=None):
+    def highlight_dates(self, calendar_widget=None):#Funckija datumu iekrāsošanai
         calendar_widget = calendar_widget or self.calendar  
         calendar_widget.calevent_remove('all')
         calendar_widget.tag_config("reminder", background="yellow", foreground="black")
@@ -149,7 +149,7 @@ class CalendarApp:#izveido klasi
         calendar_widget.update_idletasks()  
     
 
-    def save_data(self):
+    def save_data(self):#Funkcija kas saglabā informāciju .json failā
         with open(self.data_file, "w") as file:
             json.dump(self.data, file)
     
